edafdd5e287c224abaac3669a672079cbe6fe27a
sherstobitovae07 <145099828+sherstobitovae07@users.noreply.github.com>
2024-11-02 22:06:41 +0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,8 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         library library = new library();
-
-//        // Добавьте книги в библиотеку
+        
 //        Book book1 = new Book();
 //        book1.setId("1");
 //        book1.setTitle("Book Title 1");
